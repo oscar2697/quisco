c2945f11eedbcad9726445bbb91b5d2e40686d80
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-09-17 14:25:06 -0500

    "Removed font files and order page component, updated OrderSidebar component to fetch categories from Prisma and display them as CategoryIcon elements"

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,8 +1,23 @@
+import { prisma } from "@/src/lib/prisma"
+import CategoryIcon from "../ui/CategoryIcon"
 
-const OrderSidebar = () => {
+async function getCategories() {
+    return await prisma.category.findMany()
+}
+
+async function OrderSidebar () {
+    const categories = await getCategories()
+
     return (
         <aside className="md:w-72 md:h-screen bg-white">
-            Order Sidebar
+            <nav className="mt-10">
+                {categories.map(category => (
+                    <CategoryIcon
+                        key={category.id}
+                        category={category}
+                    />
+                ))}
+            </nav>
         </aside>
     )
 }
