5f6f2594f1da708507267c4ce85d8ca9572a2a81
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-09-17 14:57:14 -0500

    "Added product fetching and display to OrderPage, and updated CategoryIcon to highlight current category"

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,8 +1,34 @@
+import ProductCard from "@/components/products/ProductCard"
+import { prisma } from "@/src/lib/prisma"
 
-export default function OrderPage ({params}: {params: {category: string}}) {
+async function getProducts(category:string) {
+    const products = await prisma.product.findMany({
+        where: {
+            category: {
+                slug: category
+            }
+        }
+    })
+    return products
+}
+
+export default async function OrderPage ({params}: {params: {category: string}}) {
+    const products = await getProducts(params.category)
+
     return (
-        <div>
-
-        </div>
+        <>
+            <h1 className="text-2xl my-10">
+                Elige Tu pedido
+            </h1>
+            
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+                {products.map(product => (
+                    <ProductCard
+                        key={product.id}
+                        product={product}
+                    />
+                ))}
+            </div>
+        </>
     )
 }
