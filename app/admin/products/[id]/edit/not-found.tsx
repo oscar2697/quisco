52a7253021850678af5773a887dfb30b41e18739
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-09-27 17:59:58 -0500

    "Update OrderPage grid layout, add image display to ImageUpload, and modify ProductForm to accept product prop and display existing values"

---
+++ app/admin/products/[id]/edit/not-found.tsx
@@ -0,0 +1,18 @@
+import Heading from '@/components/ui/Heading'
+import Link from 'next/link'
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div className='text-center'>
+            <Heading>Producto No Encontrado</Heading>
+
+            <Link
+                href='/admin/products'
+                className='bg-amber-400 text-black px-10 py-3 text-xl text-center font-bold cursor-pointer w-full lg:w-auto'
+            >
+                Ir a Producto
+            </Link>
+        </div>
+    )
+}
