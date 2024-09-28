d7cc261704f5521d0d8d49350653318809b26b70
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-09-27 19:26:41 -0500

    "Refactor admin/orders/page.tsx and app/page.tsx: removed error handling, updated loading indicator, and modified app page to redirect to /order/cafe"

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -9,17 +9,16 @@ export default function OrdersPage() {
     const url = '/admin/orders/api'
     const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
 
-    const {data, error, isLoading} = useSWR<OrderWithProducts[]>(url, fetcher, {
+    const {data, isLoading} = useSWR<OrderWithProducts[]>(url, fetcher, {
         refreshInterval: 1000,
         revalidateOnFocus: false
     })
 
-    if(isLoading) return 'Cargando...'
+    if(isLoading) return <p>Cargando...</p>
 
     if(data) return (
         <>
             <Heading>Administrar Ordenes</Heading>
-
 
             {data.length ? (
                 <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 mt-5'>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <h1>Hola Mundo</h1>
-  );
+  redirect('/order/cafe')
 }
